b0d56cf7b5d2778cb150f6ab06cc9b37cd0fe25a
alberto.gilf <alberto.gilf@gmail.com>
2019-02-27 16:13:51 +0100

    bad formatting but pdf generated

--- htmltopdf.py
+++ htmltopdf.py
@@ -1,15 +1,16 @@
 import pdfkit
 
-css = 'style.css'
+
 options={
-	"margin-top":"0in",
-	"margin-bottom": "0in",
-	"margin-left": "0in",
-	"margin-right":"0in",
+	"margin-top":"0",
+	"margin-bottom": "0",
+	"margin-left": "0",
+	"margin-right":"0",
 	"page-height":"11in",
 	"page-width":"9in",
 	"encoding":"UTF-8",
 	"print-media-type":"",
 	"no-stop-slow-scripts":""
 }
+
 pdfkit.from_file('cv_GildelaFuenteAlberto.html','cv_GildelaFuenteAlberto.pdf',options=options)
